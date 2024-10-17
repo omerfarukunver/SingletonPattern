7209b2d061f14693851654a120cbd17fd76692f9
galat <galat@UNOVERY>
2024-10-17 08:46:28 +0300

    Proje dosyası ekle.

---
+++ SingletonPattern/LogManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class LogManager
+    {
+        private static LogManager _instance;
+
+        // Private constructor, dışarıdan nesne oluşturulamaz. Ondan dolayı boş
+        private LogManager() {}
+
+        // Tek örneği almak için metod
+        public static LogManager GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new LogManager();
+            }
+            return _instance;
+        }
+
+        // Log işlemi, sadece Console'a yazdırıyor
+        public void Log(string message)
+        {
+            Console.WriteLine($"Log atıldı: {message}");
+        }
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // LogManager'ın örneğini alıyoruz
+            LogManager logger = LogManager.GetInstance();
+
+            // Log işlemi
+            logger.Log("Program başladı.");
+            logger.Log("İlk kayıt atıldı.");
+
+            // Aynı singleton instance ile farklı bir log işlemi
+            LogManager anotherLogger = LogManager.GetInstance();
+            anotherLogger.Log("İkinci kayıt atıldı.");
+
+            // İki referansın aynı olup olmadığının kontrolü
+            Console.WriteLine(Object.ReferenceEquals(logger, anotherLogger)); 
+        }
+    }
+}
